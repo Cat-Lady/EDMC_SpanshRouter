902fd01e1162c816c6931d41499110f122cd6c75
Cat Lady <twilight312@gmail.com>
2025-12-05 13:45:34 +0100

    Ensured distances are shown to the right of previous, current, and next waypoint buttons - instead of in new lines under those.

--- SpanshRouter/SpanshRouter.py
+++ SpanshRouter/SpanshRouter.py
@@ -101,17 +101,14 @@ class SpanshRouter():
         self.clear_route_btn = tk.Button(self.frame, text="Clear route", command=self.clear_route)
 
         row = 0
-        self.waypoint_prev_btn.grid(row=row, columnspan=2)
+        self.waypoint_prev_btn.grid(row=row, column=0, padx=5, pady=10, sticky=tk.W)
+        self.dist_prev_lbl.grid(row=row, column=1, padx=5, pady=10, sticky=tk.W)
         row += 1
-        self.waypoint_btn.grid(row=row, columnspan=2)
+        self.waypoint_btn.grid(row=row, column=0, padx=5, pady=10, sticky=tk.W)
+        self.dist_remaining_lbl.grid(row=row, column=1, padx=5, pady=10, sticky=tk.W)
         row += 1
-        self.waypoint_next_btn.grid(row=row, columnspan=2)
-        row += 1
-        self.dist_prev_lbl.grid(row=row, columnspan=2)
-        row += 1
-        self.dist_next_lbl.grid(row=row, columnspan=2)
-        row += 1
-        self.dist_remaining_lbl.grid(row=row, columnspan=2)
+        self.waypoint_next_btn.grid(row=row, column=0, padx=5, pady=10, sticky=tk.W)
+        self.dist_next_lbl.grid(row=row, column=1, padx=5, pady=10, sticky=tk.W)
         row += 1
         self.bodies_lbl.grid(row=row, columnspan=2, sticky=tk.W)
         row += 1
